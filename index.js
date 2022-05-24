6d8ccaa8c7a026caa2539ba7fb4e3783c43fe966
Luis <lvas248@gmail.com>
2022-05-24 15:04:49 -0400

    commit tree cff7b8898ccadf8d4c10977e57906b03cf3aab5b

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution here
+
+function findMatching(array, string){
+    return array.filter(function (name){
+        return name.toUpperCase() === string.toUpperCase()})
+    }
+
+function fuzzyMatch(array, string){
+    return array.filter(function (names){
+        return names.substring(0,string.length).toUpperCase() === string.toUpperCase()})
+}   
+
+function matchName(array, string){
+    return array.filter(function(obj){
+        return obj.name.toUpperCase() === string.toUpperCase();
+    })  
+}
